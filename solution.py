5595f231077240842f9ebed040eb82a6c85109cb
igolnikol <129745179+igolnikol@users.noreply.github.com>
2023-04-13 10:34:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+import math
+import scipy.stats as stats
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 263819966 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +14,15 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    px = x_success / x_cnt
+    py = y_success / y_cnt  
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt) 
+    difference = px - py
+    
+    z_value = difference / math.sqrt(p_combined * (1 - p_combined) * (1/x_cnt + 1/y_cnt))
+    distr = stats.norm(0, 1)
+    
+    p_value = (1 - distr.cdf(abs(z_value))) * 2
+    
+    return p_value < alpha # Ваш ответ, True или False
